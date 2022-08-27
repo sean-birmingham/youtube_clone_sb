2856edeb675325fb266377daf7f3b6b87f2f6932
Sean Birmingham <sean.birmingham98@gmail.com>
2022-08-27 14:03:44 -0400

    displaying video card and channel card

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,7 +2,12 @@ import { Stack, Box } from '@mui/material';
 import { VideoCard, ChannelCard } from './';
 
 const Videos = ({ videos }) => (
-  <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
+  <Stack
+    direction="row"
+    flexWrap="wrap"
+    justifyContent="start"
+    alignItems="start"
+    gap={2}>
     {videos.map((item, idx) => (
       <Box key={idx}>
         {item.id.videoId && <VideoCard video={item} />}
