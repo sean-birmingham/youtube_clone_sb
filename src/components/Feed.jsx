bc0af588ea34a5c6b5b2727912d54171a8d2ef3a
Sean Birmingham <sean.birmingham98@gmail.com>
2022-08-27 12:57:25 -0400

    sidebar component set up

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,25 @@
+import { useState, useEffect } from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+import Sidebar from './Sidebar';
+
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <Stack sx={{ flexDirection: { xs: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { xs: 'auto', md: '92vh' },
+          borderRight: '1px solid #3d3d3d',
+          px: { xs: 0, md: 2 },
+        }}>
+        <Sidebar />
+        <Typography
+          variant="body2"
+          className="copyright"
+          sx={{ mt: 1.5, color: '#fff' }}>
+          Copyright &copy; 2022 SB Media
+        </Typography>
+      </Box>
+    </Stack>
+  );
 };
 export default Feed;
